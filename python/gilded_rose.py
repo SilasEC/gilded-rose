bda4ff70e4b916bb8fae5867c577892422795fe0
SilasEC <silas.curtis@cune.org>
2025-03-20 23:44:35 -0500

    Refactored code.

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -1,42 +1,37 @@
-# -*- coding: utf-8 -*-
+"""
+Gilded Rose Refactored
+
+This script implements an object-oriented solution for managing 
+inventory items in the Gilded Rose system. Different item types 
+depreciate or appreciate in quality based on specific rules.
+
+Classes:
+- GildedRose: Handles bulk updates of item quality.
+- Item: Base class for all items.
+- Default: Standard items that degrade over time.
+- Aged: Items that improve in quality over time.
+- Legendary: Items that do not change in quality.
+- Ticket: Items that increase in quality until expiration.
+- Conjured: Items that degrade in quality at an accelerated rate.
+
+Author: Silas Curtis
+Date: 3.20.25
+"""
 
 class GildedRose(object):
+    """Creates a class for the store, and when called will update all items in the store for the next day."""
 
     def __init__(self, items):
         self.items = items
 
     def update_quality(self):
         for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
-            else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
-            if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
-                else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
-
+            item.update_date()
+            item.update_quality()
 
 class Item:
+    """Create parent class for all items to be sold."""
+
     def __init__(self, name, sell_in, quality):
         self.name = name
         self.sell_in = sell_in
@@ -44,3 +39,80 @@ class Item:
 
     def __repr__(self):
         return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
+
+class Default (Item):
+    """Create class for default items, which depreciate over time, and faster after sell by date."""
+
+    def __init__(self, name, sell_in, quality):
+        super().__init__(name, sell_in, quality)
+
+    def update_date(self):
+        self.sell_in = self.sell_in-1
+    
+    def update_quality(self):
+        if self.sell_in > 0:
+            self.quality = max(self.quality-1, 0)
+        else:
+            self.quality = max(self.quality-2, 0)
+        
+
+class Aged (Item):
+    """Create class for aged items, which appreciate over time."""
+
+    def __init__(self, name, sell_in, quality):
+        super().__init__(name, sell_in, quality)
+
+    def update_date(self):
+        self.sell_in = self.sell_in - 1
+    
+    def update_quality(self):
+        self.quality = min(self.quality+1, 50)
+
+
+class Legendary (Item):
+    """Create class for legendary items, which effectively have no sell by date and do not change in quality."""
+
+    def __init__(self, name, sell_in, quality):
+        super().__init__(name, sell_in, quality)
+
+    def update_date(self):
+        self.sell_in = self.sell_in
+    
+    def update_quality(self):
+        self.quality = self.quality
+
+class Ticket (Item):
+    """Create class for tickets, whose quality changes at different rates."""
+
+    def __init__(self, name, sell_in, quality):
+        super().__init__(name, sell_in, quality)
+
+    def update_date(self):
+        self.sell_in = self.sell_in-1
+    
+    def update_quality(self):
+
+        if self.sell_in < 0:
+            self.quality = 0
+
+        elif self.sell_in <= 5:
+            self.quality = min(self.quality + 3, 50)
+
+        elif self.sell_in <=10:
+            self.quality = min(self.quality+2,50)
+
+        else:
+            self.quality = min(self.quality + 1, 50)
+        
+
+class Conjured (Item):
+    """Create class for conjured items, which depreciate quickly."""
+
+    def __init__(self, name, sell_in, quality):
+        super().__init__(name, sell_in, quality)
+
+    def update_date(self):
+        self.sell_in = self.sell_in-1
+
+    def update_quality(self):
+        self.quality = max(self.quality-2, 0)
--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -1,16 +1,74 @@
-# -*- coding: utf-8 -*-
+"""
+Unit Tests for Gilded Rose Inventory Management
+
+This script contains unit tests for the Gilded Rose inventory system, 
+ensuring that different item types update their quality and sell-in 
+values according to the specified rules.
+
+Author: Silas Curtis
+Date: 3.20.25
+"""
 import unittest
 
-from gilded_rose import Item, GildedRose
+from gilded_rose import Item, GildedRose, Default, Aged, Ticket, Legendary, Conjured
 
 
 class GildedRoseTest(unittest.TestCase):
-    def test_foo(self):
-        items = [Item("foo", 0, 0)]
+    """Create all tests."""
+    
+    def test_default(self):
+        items = [Default("cake", 20, 40)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        self.assertEqual("fixme", items[0].name)
+        self.assertEqual(39, items[0].quality)
 
-        
+    def test_default_after_sell_by(self):
+        items = [Default("pie", -1, 40)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(38, items[0].quality)
+
+    def test_aged(self):
+        items = [Aged("wine", 20, 40)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(41, items[0].quality)
+    
+    def test_aged_ceiling(self):
+        items = [Aged("vinegar", 20, 50)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(50, items[0].quality)
+
+    def test_ticket(self):
+        items = [Ticket("sam smith concert", 20, 40)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(41, items[0].quality)
+
+    def test_ticket_five_or_less(self):
+        items = [Ticket("tom mcdonald concert", 2, 40)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(43, items[0].quality)
+
+    def test_ticket_after_sell_by(self):
+        items = [Ticket("jonas brothers concert", 0, 40)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(0, items[0].quality)
+
+    def test_conjured(self):
+        items = [Conjured("pet genie", 8, 40)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(38, items[0].quality)
+
+    def test_legendary(self):
+        items = [Legendary("big chungus body pillow", 20, 40)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(40, items[0].quality)
+
 if __name__ == '__main__':
     unittest.main()
